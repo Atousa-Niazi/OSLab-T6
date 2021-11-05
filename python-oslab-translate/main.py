3a4babc0d9fe04ec7bda996b0dacb1b18751c732
Atousa Niazi <91827654+Atousa-Niazi@users.noreply.github.com>
2021-11-05 15:04:17 +0330

    Update main.py

--- python-oslab-translate/main.py
+++ python-oslab-translate/main.py
@@ -3,7 +3,15 @@
 # dar python or /else darim  :  dar for break ejra she else ejra nemy she va baraks
 
 WORDS_BANK=[]
-
+#############################
+def file_check():
+    try:
+        with open('words_bank.txt','r') as f:
+            print('file exist.')
+    except:
+        print("\033[38;5;202mFile not found. Check the path variable and filename")
+        exit()
+#############################
 def load_data():
     #f=open('words_bank.txt','r')
     with open('words_bank.txt','r') as f :
@@ -14,12 +22,11 @@ def load_data():
             WORDS_BANK.append({'english':words[i],'persian':words[i+1]})
             
     for w in WORDS_BANK:
-        print(w)
-    print('loaded!')
-
-
+        print("\033[38;5;204m",w)
+    print('\033[38;5;118mloaded!')
+#############################
 def add():
-    print("\033[38;5;76madd a new word:")
+    print("\033[38;5;215madd a new word:")
     print("to add a new item: ")
     print('english:')
     en_in=input()
@@ -27,38 +34,47 @@ def add():
     fa_in=input()
     adding=[en_in,fa_in]
     WORDS_BANK.append({'english':adding[0],'persian':adding[1]})
-
-
-
+    print('do you want to save the new word? (y or n) ')
+    answer=input()
+    if answer=='y':
+        save()
+#############################
 def input_text_user():
     user_text=input('\033[38;5;78mplease write your text: ')
     return user_text
-    
-def en2fa(input_text):
-    user_words=user_text.split(' ')
-    output_text=''
-    for user_word in user_words:
-        for word in WORDS_BANK:
-            if user_word == word['english']:
-                output_text+=word['persian']+' '
-                break
-        else:
-            output_text+=user_word+' '
-    return output_text
-    
+#############################
+def en2fa(user_text):
+    output_sentence=''
+    user_sentences=user_text.split('.')
+    for user_sentence in user_sentences:
+        user_words=user_sentence.split(' ')
+        output_text=''
+        for user_word in user_words:
+            for word in WORDS_BANK:
+                if user_word == word['english']:
+                    output_text+=word['persian']+' '
+                    break
+            else:
+                output_text+=user_word+' '
+        output_sentence+=output_text
+    return output_sentence
+#############################
 def fa2en(user_text):
-    user_words=user_text.split(' ')
-    output_text=''
-    for user_word in user_words:
-        for word in WORDS_BANK:
-            if user_word == word['persian']:
-                output_text+=word['english']+' '
-                break
-        else:
-            output_text+=user_word+' '
-    return output_text
-
-
+    output_sentence=''
+    user_sentences=user_text.split('.')
+    for user_sentence in user_sentences:
+        user_words=user_sentence.split(' ')
+        output_text=''
+        for user_word in user_words:
+            for word in WORDS_BANK:
+                if user_word == word['persian']:
+                    output_text+=word['english']+' '
+                    break
+            else:
+                output_text+=user_word+' '
+        output_sentence+=output_text
+    return output_sentence
+#############################
 def show_menu():
     print("\033[1;95m~~~~~~~~~~~~~~~~~~~~~~")
     print('Welcome to Sadjad Translator!')
@@ -66,38 +82,38 @@ def show_menu():
     print('2- Translation english2persian')
     print('3- Translation persian2english')
     print('4- Exit')
-    print("\033[1;95m~~~~~~~~~~~~~~~~~~~~~~\n")
-
+    print("~~~~~~~~~~~~~~~~~~~~~~\n")
+#############################
 def save():
-    print('saved')
     f=open('words_bank.txt','w') # i opened the file with w to prevent overwritting
-    result = str(WORDS_BANK)
-    for row in result:
-        f.write(row+'\n')
+    for row in WORDS_BANK:
+        f.write(row['english']+'\n'+row['persian'])
     f.close()
-    print()
-    print()
+    print('\033[38;5;222msaved')
+    print('\033[38;5;220m.......................')
+#############################
 
-print("\033[38;5;78mloading... \n")
+print("\033[38;5;118mloading...")
+file_check()
 load_data()
-print("\033[38;5;78m\ndatabase loaded. app is ready to use.\n")
+print("\033[38;5;220mdatabase loaded. app is ready to use.\n")
 while True:
     show_menu()
-    print("\033[38;5;78mEnter your choice:")
+    print("\033[38;5;33mEnter your choice:")
     choice=int(input())
     
     if choice==1:
         add()
-        save()
     elif choice==2:
         user_text=input_text_user()
-        output_text=en2fa(user_text)
+        output_sentence=en2fa(user_text)
         print("\033[38;5;78myour Translated text is :")
-        print("\033[38;5;215m",output_text)
+        print("\033[38;5;215m",output_sentence)
     elif choice==3:
         user_text=input_text_user()
-        output_text=fa2en(user_text)
-        print("\033[38;5;78myour Translated text is :")
-        print("\033[38;5;215m",output_text)
+        output_sentence=fa2en(user_text)
+        print("\033[38;5;93myour Translated text is :")
+        print("\033[38;5;198m",output_sentence)
     elif choice==4:
+        print('good luck.')
         break
